bf01cf4c2ca6381032fc779754b5a2034adb51f5
lgh1891 <lgh1891@naver.com>
2023-07-27 17:32:56 +0900

    0727

--- src/main/java/com/kiosk/app/infra/menu/MenuController.java
+++ src/main/java/com/kiosk/app/infra/menu/MenuController.java
@@ -8,8 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.kiosk.app.infra.member.Member;
-
 
 @Controller
 public class MenuController {
