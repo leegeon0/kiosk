0d967a1abbaca5e9faec3f70b57dd61901c25156
burnttoastcrumbs <mc1818@naver.com>
2023-08-08 14:10:37 +0900

    customer, stamp 커밋

--- src/main/java/com/kiosk/app/infra/stamp/Stamp.java
+++ src/main/java/com/kiosk/app/infra/stamp/Stamp.java
@@ -1,46 +1,105 @@
 package com.kiosk.app.infra.stamp;
 
-public class Stamp {
-	private String seq;
-	private String stampNy;
-	private String countStamp;
-	private String countStampUseDate;
-	private String customer_seq;
-	
 
-	
+
+public class Stamp {
+
+	private String stampSeq;
+
+	private String stampNy;
+
+	private String countStamp;
+
+	private String countStampUseDate;
+
+	private String customer_seq;
+
+
+
+
+
+
+
 //	----------------------------------------
-	
-	
-	public String getSeq() {
-		return seq;
-	}
-	public void setSeq(String seq) {
-		this.seq = seq;
-	}
+
+
+
+
+
+
+
 	public String getStampNy() {
+
 		return stampNy;
+
 	}
-	public void setStampNy(String stampNy) {
-		this.stampNy = stampNy;
+
+	public String getStampSeq() {
+
+		return stampSeq;
+
 	}
-	public String getCountStamp() {
-		return countStamp;
+
+	public void setStampSeq(String stampSeq) {
+
+		this.stampSeq = stampSeq;
+
 	}
-	public void setCountStamp(String countStamp) {
-		this.countStamp = countStamp;
-	}
-	public String getCountStampUseDate() {
-		return countStampUseDate;
-	}
-	public void setCountStampUseDate(String countStampUseDate) {
-		this.countStampUseDate = countStampUseDate;
-	}
-	public String getCustmer_seq() {
+
+	public String getCustomer_seq() {
+
 		return customer_seq;
+
 	}
+
+	public void setCustomer_seq(String customer_seq) {
+
+		this.customer_seq = customer_seq;
+
+	}
+
+	public void setStampNy(String stampNy) {
+
+		this.stampNy = stampNy;
+
+	}
+
+	public String getCountStamp() {
+
+		return countStamp;
+
+	}
+
+	public void setCountStamp(String countStamp) {
+
+		this.countStamp = countStamp;
+
+	}
+
+	public String getCountStampUseDate() {
+
+		return countStampUseDate;
+
+	}
+
+	public void setCountStampUseDate(String countStampUseDate) {
+
+		this.countStampUseDate = countStampUseDate;
+
+	}
+
+	public String getCustmer_seq() {
+
+		return customer_seq;
+
+	}
+
 	public void setCustmer_seq(String custmer_seq) {
+
 		this.customer_seq = custmer_seq;
+
 	}
-	
+
+
+
 }
