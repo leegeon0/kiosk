5ea47025608a75effef3a8cd82da1c866f800e0c
lgh1891 <lgh1891@naver.com>
2023-08-08 17:33:14 +0900

    optionMenu 진행중

---
+++ src/main/java/com/kiosk/app/infra/optionMenu/OptionMenu.java
@@ -0,0 +1,43 @@
+package com.kiosk.app.infra.optionMenu;
+
+public class OptionMenu {
+	private String optionMenuSeq;
+	private String optionMenuImg;
+	private String optionMenuName;
+	private String optionMenuPrice;
+	
+	
+//	----------	
+	
+	
+	
+	public String getOptionMenuSeq() {
+		return optionMenuSeq;
+	}
+	public void setOptionMenuSeq(String optionMenuSeq) {
+		this.optionMenuSeq = optionMenuSeq;
+	}
+	public String getOptionMenuImg() {
+		return optionMenuImg;
+	}
+	public void setOptionMenuImg(String optionMenuImg) {
+		this.optionMenuImg = optionMenuImg;
+	}
+	public String getOptionMenuName() {
+		return optionMenuName;
+	}
+	public void setOptionMenuName(String optionMenuName) {
+		this.optionMenuName = optionMenuName;
+	}
+	public String getOptionMenuPrice() {
+		return optionMenuPrice;
+	}
+	public void setOptionMenuPrice(String optionMenuPrice) {
+		this.optionMenuPrice = optionMenuPrice;
+	}
+	
+	
+
+	
+	
+}
