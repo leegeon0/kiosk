552d39892ebcdfea36510e456be19301e692b34d
lgh1891 <lgh1891@naver.com>
2023-08-30 16:14:59 +0900

    orderList 가져오기 mapper 진행중 08.30 geon

--- src/main/java/com/kiosk/app/infra/orderList/OrderList.java
+++ src/main/java/com/kiosk/app/infra/orderList/OrderList.java
@@ -2,34 +2,52 @@ package com.kiosk.app.infra.orderList;
 
 public class OrderList {
 	private String orderListSeq;
-	private String optionList_seq;
-	private String menuList_seq;
+	private String menu_seq;
+	private String optionMenu_optionMenuSeq;
+	private String menuQuantity;
+	private String optionQuantity;
+	private String delNy;
 	
 	
-//	----------------------------------------
 
+//	----------------------------------------
 
 	public String getOrderListSeq() {
 		return orderListSeq;
 	}
-
 	public void setOrderListSeq(String orderListSeq) {
 		this.orderListSeq = orderListSeq;
 	}
-
-	public String getOptionList_seq() {
-		return optionList_seq;
+	public String getMenu_seq() {
+		return menu_seq;
 	}
-
-	public void setOptionList_seq(String optionList_seq) {
-		this.optionList_seq = optionList_seq;
+	public void setMenu_seq(String menu_seq) {
+		this.menu_seq = menu_seq;
 	}
-
-	public String getMenuList_seq() {
-		return menuList_seq;
+	public String getOptionMenu_optionMenuSeq() {
+		return optionMenu_optionMenuSeq;
 	}
-	public void setMenuList_seq(String menuList_seq) {
-		this.menuList_seq = menuList_seq;
+	public void setOptionMenu_optionMenuSeq(String optionMenu_optionMenuSeq) {
+		this.optionMenu_optionMenuSeq = optionMenu_optionMenuSeq;
 	}
+	public String getMenuQuantity() {
+		return menuQuantity;
+	}
+	public void setMenuQuantity(String menuQuantity) {
+		this.menuQuantity = menuQuantity;
+	}
+	public String getOptionQuantity() {
+		return optionQuantity;
+	}
+	public void setOptionQuantity(String optionQuantity) {
+		this.optionQuantity = optionQuantity;
+	}
+	public String getDelNy() {
+		return delNy;
+	}
+	public void setDelNy(String delNy) {
+		this.delNy = delNy;
+	}
+	
 	
 }
--- src/main/java/com/kiosk/app/infra/orderList/OrderListVo.java
+++ src/main/java/com/kiosk/app/infra/orderList/OrderListVo.java
@@ -28,8 +28,11 @@ public class OrderListVo {
 	private String shKeyword;
 
 	private String orderListSeq;
-	private String optionList_seq;
-	private String menuList_seq;
+	private String menu_seq;
+	private String optionMenu_optionMenuSeq;
+	private String menuQuantity;
+	private String optionQuantity;
+	private String delNy;
 // ----------
 	public Integer getShOption() {
 		return shOption;
@@ -46,22 +49,38 @@ public class OrderListVo {
 		this.orderListSeq = orderListSeq;
 	}
 
-	public String getOptionList_seq() {
-		return optionList_seq;
-	}
+	
 
-	public void setOptionList_seq(String optionList_seq) {
-		this.optionList_seq = optionList_seq;
+	public String getMenu_seq() {
+		return menu_seq;
 	}
-
-	public String getMenuList_seq() {
-		return menuList_seq;
+	public void setMenu_seq(String menu_seq) {
+		this.menu_seq = menu_seq;
 	}
-
-	public void setMenuList_seq(String menuList_seq) {
-		this.menuList_seq = menuList_seq;
+	public String getOptionMenu_optionMenuSeq() {
+		return optionMenu_optionMenuSeq;
 	}
-
+	public void setOptionMenu_optionMenuSeq(String optionMenu_optionMenuSeq) {
+		this.optionMenu_optionMenuSeq = optionMenu_optionMenuSeq;
+	}
+	public String getMenuQuantity() {
+		return menuQuantity;
+	}
+	public void setMenuQuantity(String menuQuantity) {
+		this.menuQuantity = menuQuantity;
+	}
+	public String getOptionQuantity() {
+		return optionQuantity;
+	}
+	public void setOptionQuantity(String optionQuantity) {
+		this.optionQuantity = optionQuantity;
+	}
+	public String getDelNy() {
+		return delNy;
+	}
+	public void setDelNy(String delNy) {
+		this.delNy = delNy;
+	}
 	public String getShKeyword() {
 		return shKeyword;
 	}
